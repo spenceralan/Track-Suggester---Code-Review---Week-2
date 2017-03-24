f3f181394edc0d5874e99badbcfb0a9c221822fd
Spencer A Ruiz <spencer.alan@icloud.com>
2017-03-24 09:25:33 -0700

    hide start quiz page and show questions when start quiz button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(function() {
-
-  $("#startQuiz").show();
   $("#questions").hide();
   $("#results").hide();
+  $("#startQuiz").show();
 
+  $("#startButton").click(function() {
+    $("#startQuiz").hide();
+    $("#questions").show();
+  });
 
+  
 });
