57bfb12816e81f118bf037434a47035914c0fab2
Spencer A Ruiz <spencer.alan@icloud.com>
2017-03-24 10:16:58 -0700

    add next and back buttons that hide and show questions

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,27 @@ $(function() {
   $("#startButton").click(function() {
     $("#startQuiz").hide();
     $("#questions").show();
+    $("#question1").show();
   });
 
-  
+  $("#nextButton1").click(function() {
+    $("#question1").hide();
+    $("#question2").show();
+  });
+
+  $("#nextButton2").click(function() {
+    $("#question2").hide();
+    $("#question3").show();
+  });
+
+  $("#backButton1").click(function() {
+    $("#question2").hide();
+    $("#question1").show();
+  });
+
+  $("#backButton2").click(function() {
+    $("#question3").hide();
+    $("#question2").show();
+  });
+
 });
